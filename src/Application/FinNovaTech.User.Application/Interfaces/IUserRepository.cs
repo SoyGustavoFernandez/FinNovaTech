6d9df0f04296f83b8d33922e6e80956930525fad
Gustavo Fernández Saavedra <u22243779@utp.edu.pe>
2025-03-19 21:14:13 -0500

    Feature: Actualización de referencias

--- src/Application/FinNovaTech.User.Application/Interfaces/IUserRepository.cs
+++ src/Application/FinNovaTech.User.Application/Interfaces/IUserRepository.cs
@@ -1,16 +1,16 @@
 ﻿using FinNovaTech.User.Application.DTOs;
-using FinNovaTech.User.Domain.Entities;
+using userEntity = FinNovaTech.User.Domain.Entities;
 
 namespace FinNovaTech.User.Application.Interfaces
 {
     public interface IUserRepository
     {
         Task<bool> EmailExistsAsync(string email);
-        Task AddUserAsync(Domain.Entities.User user);
-        Task UpdateUserAsync(Domain.Entities.User user);
-        Task DeleteUserAsync(Domain.Entities.User user);
+        Task AddUserAsync(userEntity.User user);
+        Task UpdateUserAsync(userEntity.User user);
+        Task DeleteUserAsync(userEntity.User user);
         Task<UserDto> GetUserByIdAsync(int id);
-        Task<Domain.Entities.User> GetUserEntityByIdAsync(int id);
+        Task<userEntity.User> GetUserEntityByIdAsync(int id);
         Task<List<UserDto>> GetAllUsersAsync();
     }
 }
