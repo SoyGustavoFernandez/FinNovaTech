fbdb5581cae085660904b14a26cd4088d757ad20
Gustavo Fernández Saavedra <u22243779@utp.edu.pe>
2025-03-19 23:11:14 -0500

    Feature: Se agrega carpeta log

---
+++ src/Application/FinNovaTech.User.Application/Queries/Logs/GetLogByUserIdQuery.cs
@@ -0,0 +1,21 @@
+﻿using FinNovaTech.Common.Domain.Entities;
+using FinNovaTech.User.Application.DTOs;
+using MediatR;
+
+namespace FinNovaTech.User.Application.Queries.Logs
+{
+    /// <summary>
+    /// Query para obtener los logs de un usuario.
+    /// </summary>
+    /// <remarks>
+    /// Constructor.
+    /// </remarks>
+    /// <param name="userId"></param>
+    public class GetLogByUserIdQuery(int userId) : IRequest<Response<List<UserLogsDto>>>
+    {
+        /// <summary>
+        /// Id del usuario.
+        /// </summary>
+        public int UserId { get; set; } = userId;
+    }
+}
---
+++ src/Application/FinNovaTech.User.Application/Queries/Logs/Handler/GetUserLogsHandler.cs
@@ -0,0 +1,20 @@
+﻿using FinNovaTech.Common.Domain.Entities;
+using FinNovaTech.User.Application.DTOs;
+using FinNovaTech.User.Application.Interfaces;
+using MediatR;
+using System.Net;
+
+namespace FinNovaTech.User.Application.Queries.Logs.Handler
+{
+    public class GetUserLogsHandler(IUserLogRepository repository) : IRequestHandler<GetLogByUserIdQuery, Response<List<UserLogsDto>>>
+    {
+        private readonly IUserLogRepository _repository = repository;
+
+        public async Task<Response<List<UserLogsDto>>> Handle(GetLogByUserIdQuery request, CancellationToken cancellationToken)
+        {
+            var logs = await _repository.GetLogsByUserIdAsync(request.UserId);
+            var logDTOs = logs.Select(log => new UserLogsDto { Action = log.Action }).ToList();
+            return new Response<List<UserLogsDto>>(true, "Logs encontrados", logDTOs, (int)HttpStatusCode.OK);
+        }
+    }
+}
